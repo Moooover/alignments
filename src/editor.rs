69d4e03330752fcd9b7e8bf1b55db683218c17e4
Moooover <kevin.cheng319@gmail.com>
2023-03-22 09:37:56 -0700

    tfr

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,16 @@
 use crate::process::TransferFunctionResults;
+use crate::ATparams;
 use nih_plug_iced::*;
 
 use std::sync::Arc;
 
 const EDITOR_W: u32 = 200;
 const EDITOR_H: u32 = 150;
+
+struct ATeditor {
+    params: Arc<ATparams>,
+    results: Arc<TransferFunctionResults>,
+}
 
 pub(crate) fn default_state() -> Arc<IcedState> {
     IcedState::from_size(EDITOR_W, EDITOR_H)
